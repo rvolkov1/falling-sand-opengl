35b8c4a4592494739d8bf6a7df667245e043227c
spike98 <rossvolkov@gmail.com>
2024-05-11 11:11:09 -0500

    texture works

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb/stb_image.h>
+
 #include "src/ShaderClass.h"
 #include "src/VAO.h"
 #include "src/VBO.h"
@@ -24,19 +27,16 @@ GLfloat vertices[] =
 GLfloat vertices[] = 
 {
   //       COORDINATES                        /  COLORS 
-  -0.5f, -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f,  0.3f,  0.02f, // lower left corner
-  0.5f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f,  0.3f,  0.02f, // lower right corner
-  0.0f,   0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   1.0f,  0.6f,  0.32f, // upper corner
-  -0.25f, 0.5f * float(sqrt(3)) / 6,     0.0f,   0.9f,  0.45f, 0.17f, // Inner left
-  0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f,   0.9f,  0.45f, 0.17f, // inner right
-  0.0f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f,  0.3f,  0.02f // inner down 
+  -0.5f, -0.5f, 0.0f,     1.0f,   0.0f,  0.0f, 0.0f, 0.0f, // lower left corner
+  -0.5f,  0.5f, 0.0f,     0.0f,   1.0f,  0.0f, 0.0f, 1.0f, // lower right corner
+  0.5f,   0.5f, 0.0f,     0.0f,   0.0f,  1.0f, 1.0f, 1.0f, // upper corner
+  0.5f,  -0.5f, 0.0f,     1.0f,   1.0f,  1.0f, 1.0f, 0.0f, // Inner left
 };
 
 GLuint indices[] = 
 {
-  0, 3, 5, // lower triangle
-  3, 2, 4, // lower right triangle
-  5, 4, 1  // upper triangle
+  0, 2, 1, // lower triangle
+  0, 3, 2, // lower right triangle
 };
 
 
@@ -88,13 +88,43 @@ int main()
   VBO VBO1(vertices, sizeof(vertices));
   EBO EBO1(indices, sizeof(indices));
 
-  VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-  VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+  VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+  VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
   VAO1.Unbind();
   VBO1.Unbind();
   EBO1.Unbind();
 
   GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+  //Texture
+  int widthImg, heightImg, numColCh;
+  stbi_set_flip_vertically_on_load(true);
+  unsigned char* bytes = stbi_load("icon.png", &widthImg, &heightImg, &numColCh, 0);
+
+  GLuint texture;
+  glGenTextures(1, &texture);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  // float flat_color[] = {1.0f, 1.0f, 0.5f, 1.0f};
+  // glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flat_color);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  stbi_image_free(bytes);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+  shaderProgram.Activate();
+  glUniform1i(tex0Uni, 0);
 
   // set color buffer
   glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -111,11 +141,12 @@ int main()
     shaderProgram.Activate();
 
     glUniform1f(uniID, 0.5f);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
     VAO1.Bind();
 
     //glDrawArrays(GL_TRIANGLES, 0, 3);
-    glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
 
@@ -126,6 +157,7 @@ int main()
   VAO1.Delete();
   VBO1.Delete();
   EBO1.Delete();
+  glDeleteTextures(1, &texture);
   shaderProgram.Delete();
 
   glfwDestroyWindow(window);
